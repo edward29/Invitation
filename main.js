70493b9762699882602f34a5b84e129c67ff940a
edward29 <caacbayedward@gmail.com>
2022-12-07 17:40:34 +0800

    change index of center

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ const main = document.querySelector('.main');
 
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
+const center = document.querySelector('.center');
 const lower = document.querySelector('.lower');
 const lower2 = document.querySelector('.lower-2');
 const text = document.querySelector('.text');
@@ -26,55 +27,52 @@ const see = document.querySelector('#see');
 
 
 window.addEventListener('load', () => {
-    
-        // setTimeout(() => {
-        //     main.style.display = 'flex';
-        // }, 1000);
         
+    wrapper.addEventListener('click', () => {
+        you.innerHTML = "You're invited";
+        open.innerHTML = 'PLEASE OPEN';
+        open.style.border = '1px solid #004aad';
+        open.style.background = '#004aad'
+        one.style.transform = 'rotateX(90deg)';
+        one.style.transitionDelay = '0s'
+        two.style.transform = 'rotateX(180deg)';
+        two.style.transitionDelay = '0.25s';
+        main.style.transform = 'translateY(-60px)';
+        main.style.transitionDelay = '0.5s';
         wrapper.addEventListener('click', () => {
-            you.innerHTML = "You're invited";
-            open.innerHTML = 'PLEASE OPEN';
-            open.style.border = '1px solid #004aad';
-            open.style.background = '#004aad'
-            one.style.transform = 'rotateX(90deg)';
-            one.style.transitionDelay = '0s'
-            two.style.transform = 'rotateX(180deg)';
-            two.style.transitionDelay = '0.25s';
-            main.style.transform = 'translateY(-60px)';
-            main.style.transitionDelay = '0.5s';
-            wrapper.addEventListener('click', () => {
-                setInterval(() => {
-                    text.style.display = 'none';
-                    main.style.animation = 'up 2s forwards';
-                    envelope.style.animation = 'out 2s forwards';
-                    one.style.animation = 'out1 2s forwards';
-                    two.style.animation = 'out1 2s forwards';
-                    wrapper.style.background = 'transparent';
-                }, 1000);
-        
-                setInterval(() => {
-                      envelope.style.display = 'none';
-                      one.style.display = 'none';
-                      two.style.display = 'none';
-                }, 1800);
-              
-                setInterval(() => {
-                    left.style.webkitAnimation = 'swing-top-left-fwd 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both';
-                    left.style.animation = 'swing-top-left-fwd 3s ;cubic-bezier(0.250, 0.460, 0.450, 0.940) both';
-                    right.style.webkitAnimation = 'swing-top-right-fwd 3s cubic-bezier(0.250, 0.460, 0.450, 0.940) both';
-                    right.style.animation = 'swing-top-right-fwd 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both';
-                }, 2000);
-                
-                setInterval(() => {
-                    lower.style.animation = 'lower 1.5s forwards';
-                    lower2.style.animation = 'lower-2 1.5s forwards';
-                    see.innerHTML = 'See You😍';
-                }, 3000);
+            setInterval(() => {
+                text.style.display = 'none';
+                main.style.animation = 'up 2s forwards';
+                envelope.style.animation = 'out 2s forwards';
+                one.style.animation = 'out1 2s forwards';
+                two.style.animation = 'out1 2s forwards';
+                wrapper.style.background = 'transparent';
+            }, 1000);
+    
+            setInterval(() => {
+                    envelope.style.display = 'none';
+                    one.style.display = 'none';
+                    two.style.display = 'none';
+            }, 1800);
+            
+            setInterval(() => {
+                left.style.webkitAnimation = 'swing-top-left-fwd 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both';
+                left.style.animation = 'swing-top-left-fwd 3s ;cubic-bezier(0.250, 0.460, 0.450, 0.940) both';
+                right.style.webkitAnimation = 'swing-top-right-fwd 3s cubic-bezier(0.250, 0.460, 0.450, 0.940) both';
+                right.style.animation = 'swing-top-right-fwd 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both';
+            }, 2000);
+            
+            setInterval(() => {
+                lower.style.animation = 'lower 1.5s forwards';
+                lower2.style.animation = 'lower-2 1.5s forwards';
+                see.innerHTML = 'See You😍';
+            }, 3000);
 
-                // setInterval(() => {
-                //     lower.style.display = 'none';
-                // }, 4200);
-            });
+            setInterval(() => {
+                center.style.zIndex = '999';
+            }, 6000);
+            
         });
+    });
 
 });
